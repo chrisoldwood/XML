622faac607f744cbd579178011627078aba0d812
Admin <admin@chrisoldwood.com>
2008-04-18 22:16:20 +0000

    Added support for the DOCTYPE node.

--- Node.cpp
+++ Node.cpp
@@ -33,11 +33,13 @@ const tchar* Node::FormatNodeType(NodeType eType)
 {
 	switch(eType)
 	{
-		case DOCUMENT_NODE:		return TXT("Dcoument");
+		case DOCUMENT_NODE:		return TXT("Document");
 		case ELEMENT_NODE:		return TXT("Element");
 		case TEXT_NODE:			return TXT("Text");
 		case COMMENT_NODE:		return TXT("Comment");
 		case PROCESSING_NODE:	return TXT("Processing Instruction");
+		case DOCTYPE_NODE:		return TXT("Document Type");
+		default:				ASSERT_FALSE();
 	}
 
 	throw Core::InvalidArgException(Core::Fmt(TXT("Invalid NodeType passed to GetNodeTypeStr: %u"), eType));
