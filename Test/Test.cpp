7206685311c38ba86253424e87a6353d54eda539
Admin <admin@chrisoldwood.com>
2008-12-14 08:28:07 +0000

    Added support to allow single test cases to be specified on the command line.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -25,21 +25,21 @@ extern void TestXPathIterator();
 ////////////////////////////////////////////////////////////////////////////////
 //! The entry point for the test harness.
 
-int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
+int _tmain(int argc, _TCHAR* argv[])
 {
-	TEST_SUITE_BEGIN
+	TEST_SUITE_BEGIN(argc, argv)
 	{
-		TestAttributes();
-		TestCharTable();
-		TestTextNode();
-		TestCommentNode();
-		TestProcessingNode();
-		TestElementNode();
-		TestCDataNode();
-		TestDocument();
-		TestReader();
-		TestWriter();
-		TestXPathIterator();
+		TEST_CASE(TestAttributes);
+		TEST_CASE(TestCharTable);
+		TEST_CASE(TestTextNode);
+		TEST_CASE(TestCommentNode);
+		TEST_CASE(TestProcessingNode);
+		TEST_CASE(TestElementNode);
+		TEST_CASE(TestCDataNode);
+		TEST_CASE(TestDocument);
+		TEST_CASE(TestReader);
+		TEST_CASE(TestWriter);
+		TEST_CASE(TestXPathIterator);
 
 		Core::SetTestRunFinalStatus(true);
 	}
