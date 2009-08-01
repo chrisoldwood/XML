9b6e461b2527f4e05969652ed930c6120a2bea56
Admin <admin@chrisoldwood.com>
2009-08-01 23:21:19 +0000

    Updated as the tests are now self-registering.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -8,40 +8,9 @@
 #include <Core/UnitTest.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
-// The test group functions.
-
-extern void testAttributes();
-extern void testCharTable();
-extern void testTextNode();
-extern void testCommentNode();
-extern void testProcessingNode();
-extern void testElementNode();
-extern void testCDataNode();
-extern void testDocument();
-extern void testReader();
-extern void testWriter();
-extern void testXPathIterator();
-
-////////////////////////////////////////////////////////////////////////////////
 //! The entry point for the test harness.
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TEST_SUITE_BEGIN(argc, argv)
-	{
-		TEST_CASE(testAttributes);
-		TEST_CASE(testCharTable);
-		TEST_CASE(testTextNode);
-		TEST_CASE(testCommentNode);
-		TEST_CASE(testProcessingNode);
-		TEST_CASE(testElementNode);
-		TEST_CASE(testCDataNode);
-		TEST_CASE(testDocument);
-		TEST_CASE(testReader);
-		TEST_CASE(testWriter);
-		TEST_CASE(testXPathIterator);
-
-		Core::setTestRunFinalStatus(true);
-	}
-	TEST_SUITE_END
+	TEST_SUITE_MAIN(argc, argv);
 }
